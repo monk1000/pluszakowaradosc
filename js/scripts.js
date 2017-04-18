2326b50920e0b85ee094dc7ec5e7b3170121a4b0
Tomasz Kacprzak <monkhtc@gmail.com>
2017-04-18 15:42:21 +0200

    zrobiony mobile / brak tablet

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
     $(function() {  
 
         // Default
-        jQuery.scrollSpeed(200, 300);
+       // jQuery.scrollSpeed(200, 300);
         
         // Custom Easing
         jQuery.scrollSpeed(200, 300, 'easeOutCubic');
